8b4398b4cfc4efd5f9deb2bf9892d7c7c135930f
muratacarr <acarrmuratt@gmail.com>
2024-04-08 10:57:07 +0300

    base project completed

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,10 +1,30 @@
-import Catalog from '../../features/catalog/Catalog';
 import { Container, CssBaseline, ThemeProvider, createTheme } from '@mui/material';
-
-import { useState } from 'react';
 import Header from './Header';
+import { useEffect, useState } from 'react';
+import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import agent from '../api/agent';
+import { useStoreContext } from '../context/StoreContext';
+import { getCookie } from '../util/util';
+import LoadingComponent from './LoadingComponent';
 
 function App() {
+  const {setBasket} = useStoreContext();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const buyerId = getCookie('buyerId');
+    if (buyerId) {
+      agent.Basket.get()
+        .then(basket => setBasket(basket))
+        .catch(error => console.log(error))
+        .finally(() => setLoading(false));
+    } else {
+      setLoading(false);
+    }
+  }, [setBasket])
+  
   const [darkMode, setDarkMode] = useState(false);
   const palleteType = darkMode ? 'dark' : 'light';
   const theme = createTheme({
@@ -20,12 +40,15 @@ function App() {
     setDarkMode(!darkMode);
   }
 
+  if (loading) return <LoadingComponent message='Initiasing app...' />
+
   return (
     <ThemeProvider theme={theme}>
+      <ToastContainer position='bottom-right' hideProgressBar theme='colored'  />
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
-        <Catalog />
+        <Outlet />
       </Container>
     </ThemeProvider>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './app/layout/App.tsx'
-import '../src/app/layout/styles.css'
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "../src/app/layout/styles.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./app/router/Routes.tsx";
+import { StoreProvider } from "./app/context/StoreContext.tsx";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <StoreProvider>
+      <RouterProvider router={router} />
+    </StoreProvider>
   </React.StrictMode>,
 )
